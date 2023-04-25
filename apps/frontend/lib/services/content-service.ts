0e714f160942d8479375d289507d87cfcea3423a
nyomansunima <nyomansunima@gmail.com>
2023-04-25 09:38:27 +0800

    🎉 build: add the service to call explore detail

--- apps/frontend/lib/services/content-service.ts
+++ apps/frontend/lib/services/content-service.ts
@@ -1,7 +1,29 @@
 import hashNodeClient from '@lib/connection/hashnode-connection'
-import { ArticlePost } from '~/types/content'
+import sanityClient from '@lib/connection/sanity-connection'
+import { ArticlePost, ExplorationDetail } from '~/types/content'
 
 class ContentService {
+  async loadDetailExploration(slug: string): Promise<ExplorationDetail> {
+    const query = `
+      *[_type == "exploration" && slug == $slug][0]{
+        ...,
+        "thumbnail": thumbnail.asset -> url,
+        stacks[] -> {
+          "image": image.asset -> url,
+          title,
+          url,
+        },
+        "nextExploration": *[_type == "exploration" && _id > ^._id] | order(_updatedAt asc)[0]{
+          slug,
+          title,
+        }
+      }
+    `
+
+    const res = await sanityClient.fetch(query, { slug })
+    return res
+  }
+
   async getBestWeekPosts(): Promise<ArticlePost[]> {
     const query = `{
       user(username: "nyomansunima") {
