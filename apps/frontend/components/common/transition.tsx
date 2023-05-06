ef7001534d85a49f5d7e0c680ba526be12bada49
nyomansunima <nyomansunima@gmail.com>
2023-05-06 14:47:54 +0800

    🍀 update: adding the smooth transition for load pagge

--- apps/frontend/components/common/transition.tsx
+++ apps/frontend/components/common/transition.tsx
@@ -1,6 +1,7 @@
-import { FunctionComponent, ReactNode, useEffect } from 'react'
+import { FunctionComponent, ReactNode, useEffect, useRef } from 'react'
 import styles from '@styles/components/common/transition.module.scss'
 import { useRouter } from 'next/router'
+import { gsap } from 'gsap'
 
 interface TransitionProps {
   children?: ReactNode | ReactNode[]
@@ -16,14 +17,47 @@ interface TransitionProps {
 const Transition: FunctionComponent<TransitionProps> = ({
   children,
 }): JSX.Element => {
+  const transitionRef = useRef<HTMLDivElement>(null)
   const router = useRouter()
 
-  useEffect(() => {}, [router.pathname])
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap
+        .timeline({
+          delay: 0.2,
+        })
+        .to('.overlay-transition', {
+          clipPath: 'circle(5% at 50% 50%)',
+          duration: 1.2,
+          ease: 'elastic',
+        })
+        .to('.overlay-transition', {
+          clipPath: 'circle(100% at 50% 50%)',
+          duration: 0.6,
+          ease: 'none',
+        })
+        .to('.overlay-transition', {
+          clipPath: 'circle(0% at 50% 50%)',
+          duration: 0.4,
+          ease: 'none',
+        })
+        .from('.child-transition', {
+          opacity: 0,
+          ease: 'none',
+        })
+    }, transitionRef)
+
+    return () => {
+      ctx.revert()
+    }
+  }, [router.pathname])
 
   return (
     <>
-      {children}
-      <div className={`${styles.overlay} overlay`}></div>
+      <div ref={transitionRef}>
+        <div className="child-transition">{children}</div>
+        <div className={`${styles.overlay} overlay-transition`}></div>
+      </div>
     </>
   )
 }
