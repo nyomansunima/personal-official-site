311a993de5a9151c038491faae533e224a59631a
nyomansunima <nyomansunima@gmail.com>
2023-05-08 06:57:37 +0800

    👏 update: update the explore view

--- apps/frontend/pages/explore/index.tsx
+++ apps/frontend/pages/explore/index.tsx
@@ -2,7 +2,6 @@ import { GeneralLayout } from '@components/layouts'
 import Head from 'next/head'
 import { NextPageWithLayout } from '~/types/app'
 import styles from '@styles/pages/explore/explore.module.scss'
-import { OutlineButton } from '@components/buttons'
 import { ExploreItemCard, ExploreRepoItem } from '@components/explore'
 import { GetServerSideProps } from 'next'
 import { QueryClient, dehydrate, useQuery } from '@tanstack/react-query'
@@ -10,7 +9,6 @@ import contentService from '@lib/services/content-service'
 import { useEffect, useRef } from 'react'
 import useCursor from '@lib/hooks/use-cursor'
 import { gsap } from 'gsap'
-import Image from 'next/image'
 import { ContactSection } from '@components/contact'
 
 const getServerSideProps: GetServerSideProps = async () => {
@@ -43,41 +41,30 @@ const ExplorePage: NextPageWithLayout = (): JSX.Element => {
 
   useEffect(() => {
     const ctx = gsap.context(() => {
-      gsap.from('.overview-section h1', {
-        y: 200,
-        opacity: 0,
-        duration: 1.2,
-        ease: 'back',
-      })
-
       gsap
         .timeline({
-          delay: 0.2,
+          delay: 2.4,
           defaults: {
             ease: 'back',
             duration: 1.2,
           },
-          scrollTrigger: {
-            trigger: '.more-section',
-            start: 'top 75%',
-          },
         })
-        .from('.more-section .heading h2', {
+        .from('.overview-section .heading h2', {
           y: 200,
           opacity: 0,
         })
-        .from('.more-section .heading span', {
+        .from('.overview-section .heading span', {
           y: 200,
           opacity: 0,
         })
       if (reposQuery.data) {
-        gsap.from('.more-section .content .item', {
+        gsap.from('.overview-section .content .item', {
           y: 200,
           opacity: 0,
           stagger: 0.1,
           ease: 'back',
           scrollTrigger: {
-            trigger: '.more-section .content .item',
+            trigger: '.overview-section .content .item',
             start: 'top 75%',
           },
         })
@@ -94,7 +81,7 @@ const ExplorePage: NextPageWithLayout = (): JSX.Element => {
   return (
     <>
       <Head>
-        <title>Explorations | Nyoman Sunima</title>
+        <title>Project Explorations | Nyoman Sunima</title>
         <meta
           name="description"
           content="All of my work and what i build for exploring are placed in here"
@@ -103,25 +90,9 @@ const ExplorePage: NextPageWithLayout = (): JSX.Element => {
 
       <main ref={mainRef} className={styles.main}>
         <section className={`${styles.overview_section} overview-section`}>
-          <div className={styles.content}>
-            <h1 data-cursor-size="200" data-cursor-exclusion>
-              Better everyday through explorations
-            </h1>
-          </div>
-        </section>
-        <section className={styles.content_section}>
-          <div className={styles.content}>
-            {listQuery.data &&
-              listQuery.data.map((item, index) => (
-                <ExploreItemCard data={item} key={index} />
-              ))}
-          </div>
-        </section>
-
-        <section className={`${styles.more_section} more-section`}>
           <div className={`${styles.heading} heading`}>
             <h2 data-cursor-exclusion data-cursor-size="200">
-              I explore more than you know
+              Better everyday from exploration
             </h2>
             <span>
               I always try to explore everthing to ensure my quality. I always
@@ -134,6 +105,14 @@ const ExplorePage: NextPageWithLayout = (): JSX.Element => {
             {reposQuery.data &&
               reposQuery.data.map((item, index) => (
                 <ExploreRepoItem data={item} key={index} />
+              ))}
+          </div>
+        </section>
+        <section className={styles.content_section}>
+          <div className={styles.content}>
+            {listQuery.data &&
+              listQuery.data.map((item, index) => (
+                <ExploreItemCard data={item} key={index} />
               ))}
           </div>
         </section>
