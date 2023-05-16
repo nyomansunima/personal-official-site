f2ab7e86cd126399e353c2844176eed4ee176a9b
nyomansunima <nyomansunima@gmail.com>
2023-05-16 10:19:13 +0800

    😎 refactor: moving from pages into blog section

--- apps/frontend/components/blog/index.tsx
+++ apps/frontend/components/blog/index.tsx
@@ -1,3 +1,6 @@
+import BlogBreadscrumb from './blog-breadscrumb'
 import BlogCard from './blog-card'
+import BlogRelatedPost from './blog-related-post'
+import BlogRelatedPostItem from './blog-related-post-item'
 
-export { BlogCard }
+export { BlogCard, BlogRelatedPost, BlogRelatedPostItem, BlogBreadscrumb }
