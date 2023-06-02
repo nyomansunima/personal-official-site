0fe0676da1adcf14ad7239f093162ff8cd79c54c
nyomansunima <nyomansunima@gmail.com>
2023-06-02 17:39:59 +0800

    🎉 build: create the service page

---
+++ apps/web/pages/service.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="flex">
+    <!-- intro section -->
+    <section class=""></section>
+  </main>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'main',
+})
+useSeoMeta({
+  title: 'Service',
+  description: 'Explore my expertise and service offer',
+})
+</script>
