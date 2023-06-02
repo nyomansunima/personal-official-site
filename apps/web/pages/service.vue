d4729d1ee1fd53eb772b1e1394f7301350672cf6
nyomansunima <nyomansunima@gmail.com>
2023-06-02 22:11:23 +0800

    🎉 build: create the page for service

--- apps/web/pages/service.vue
+++ apps/web/pages/service.vue
@@ -1,11 +1,63 @@
 <template>
-  <main class="flex">
+  <main class="flex flex-col pt-60 pb-80 text-xl gap-80">
     <!-- intro section -->
-    <section class=""></section>
+    <section class="container mx-auto px-20">
+      <h2 class="text-6xl font-medium leading-loose w-8/12 text-center mx-auto">
+        I
+        <span
+          class="border-4 border-black dark:border-gray-200 px-10 py-4 rounded-full"
+          >crafting</span
+        >
+        app that work as solution
+      </h2>
+    </section>
+
+    <!-- service lists -->
+    <section class="flex flex-col container mx-auto px-20 gap-32">
+      <ServiceItem
+        v-for="(item, index) in list"
+        :data="item"
+        :position="index"
+        :key="index"
+      />
+    </section>
+
+    <!-- process -->
+    <section class="flex flex-col items-center container mx-auto px-20">
+      <h2 class="text-6xl font-medium w-6/12 leading-relaxed text-center">
+        Where the
+        <span
+          class="border-4 border-black dark:border-gray-200 px-10 py-4 rounded-full"
+          >great</span
+        >
+        things began
+      </h2>
+
+      <div class="grid grid-cols-2 gap-x-5 gap-y-8 px-28 mt-32">
+        <ServiceProcessItem
+          v-for="(item, index) in process"
+          :data="item"
+          :position="index"
+          :key="index"
+        />
+      </div>
+    </section>
+
+    <!-- Get in touch -->
+    <section class="flex flex-col items-center container mx-auto px-20">
+      <h2 class="text-6xl font-medium mx-auto leading-relaxed">
+        Have an idea?
+      </h2>
+      <NuxtLink to="/contact" class="text-6xl font-normal dark:text-gray-400"
+        >Tell me about it</NuxtLink
+      >
+    </section>
   </main>
 </template>
 
 <script setup lang="ts">
+import { list, process } from '~/contents/service.json'
+
 definePageMeta({
   layout: 'main',
 })
