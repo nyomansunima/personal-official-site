9e25d1da2f7d48386eab8c0f73d7c4deb2119c0b
nyomansunima <nyomansunima@gmail.com>
2023-06-02 22:12:07 +0800

    🍀 update: add all service component

---
+++ apps/web/components/service/ServiceItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="flex gap-16 mt-32"
+    :class="[position % 2 == 0 ? 'flex-row' : 'flex-row-reverse']"
+  >
+    <div
+      class="flex w-5/12 h-[560px] relative rounded-3xl overflow-hidden cursor-pointer mx-10"
+    >
+      <NuxtImg
+        :src="data.image"
+        class="flex h-full w-full object-cover transition-all duration-500 hover:scale-105"
+      />
+    </div>
+    <div class="flex flex-col w-1/2 gap-10 pt-16">
+      <h2 class="text-5xl font-medium leading-tight">
+        {{ data.title }}
+      </h2>
+
+      <p class="leading-relaxed">
+        {{ data.desc }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface ServiceItemProps {
+  position: number
+  data: {
+    image: string
+    title: string
+    desc: string
+  }
+}
+
+defineProps<ServiceItemProps>()
+</script>
---
+++ apps/web/components/service/ServiceProcessItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="flex flex-col p-6 rounded-2xl border border-gray-100 dark:border-gray-800 gap-6"
+  >
+    <h3 class="text-2xl font-medium leading-tight">
+      {{ position + 1 }}. {{ data.title }}
+    </h3>
+    <p class="text-lg leading-relaxed">
+      {{ data.desc }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface ServiceProcessItemProps {
+  position: number
+  data: {
+    title: string
+    desc: string
+  }
+}
+
+defineProps<ServiceProcessItemProps>()
+</script>
