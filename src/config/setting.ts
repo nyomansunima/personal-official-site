8726fd582cf10e14c241cf114efcc06ae26ce99b
Nyoman Sunima <nyomansunima@gmail.com>
2024-07-18 16:36:05 +0800

    👍 fix: update the deps

--- src/config/setting.ts
+++ src/config/setting.ts
@@ -3,15 +3,8 @@
  * and all of the variables
  */
 export const configuration = {
-  hygraph: {
-    url: process.env.NEXT_PUBLIC_HYGRAPH_URL!,
-  },
   app: {
-    host: process.env.NEXT_PUBLIC_APP_HOST!,
-  },
-  brevo: {
-    url: process.env.NEXT_PUBLIC_BREVO_URL!,
-    apiKey: process.env.NEXT_PUBLIC_BREVO_API_KEY!,
+    host: process.env.NEXT_PUBLIC_APP_HOST || 3000,
   },
   google: {
     analytics: {
